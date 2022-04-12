b4e2d98d463bbec103c9769a434f33c4feeea61f
Eric Spencer <spencer22eric@gmail.com>
2022-04-11 21:50:40 -0400

    finished product

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
 const inquirer = require("inquirer");
-const generateMarkdown = require('./utils/generateMarkdown.js')
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 
 // TODO: Create an array of questions for user input
 const questions = () => {
@@ -20,7 +22,51 @@ const questions = () => {
         },
         {
             type: 'input',
-            name: 'project',
+            name: 'github',
+            message: 'What is your GitHub username?',
+            validate: gitInput => {
+                if (gitInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your GitHub username!");
+                    return false;
+                }
+            }
+        },
+        {
+            input: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your email!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmLinkedIn',
+            message: 'Would you like to add your LinkedIn profile?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'linkedIn',
+            message: 'Please paste your FULL LinkedIn URL (with https://).',
+            when: ({ confirmLinkedIn }) => {
+                if (confirmLinkedIn) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'title',
             message: 'What is your project title?',
             validate: projectTitle => {
                 if (projectTitle) {
@@ -45,33 +91,49 @@ const questions = () => {
             }
         },
         {
-            type: 'confirm',
-            name: 'confirmInstallGuide',
-            message: 'Does your project require an installation guide?',
-            default: true
-        },
-        {
             type: 'input',
             name: 'installation',
             message: 'Please enter your installation steps.',
-            when: ({ confirmInstallGuide }) => {
-                if (confirmInstallGuide) {
+            vadlidate: installationEntry => {
+                if (installationEntry) {
                     return true;
                 } else {
+                    console.log("Please enter an installation guide!")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'userGuide',
+            message: 'Please describe how to use your application.',
+            validate: usageGuide => {
+                if (usageGuide) {
+                    return true;
+                } else {
+                    console.log("Please provide a usage guide!");
                     return false;
                 }
             }
         }        
     ])
+        .then((answers) => {
+            console.log(answers);
+            writeToFile('Generated-README.md', answers);
+        })
 }
 
-questions();
-
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), err => {
+
+    });
+};
 
 // // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    questions();
+}
 
 // // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,12 +14,50 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `## License   
+  \n   
+  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/). ${renderLicenseLink(license)}. ${license}`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+
+${data.description}
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.userGuide}
+
+## Credits
+
+- a list of contributors goes here!
+
+## Questions?
+
+- [GitHub](https://github.com/${data.github})
+- [LinkedIn](${data.linkedIn})
+- [${data.email}](mailto:${data.email})
+
+${renderLicenseSection(data.license)}
+
+${renderLicenseBadge(data.license)}
 `;
 }
 
